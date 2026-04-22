3857ed3b23ce977d76b45267e9146b08c57101c7
Jeephoenix <168826535+Jeephoenix@users.noreply.github.com>
2026-04-22 16:21:15 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ const basescanKey = process.env.BASESCAN_API_KEY || "";
 module.exports = {
   solidity: {
     version: "0.8.20",
-    settings: { optimizer: { enabled: true, runs: 200 } },
+    settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true },
   },
   networks: {
     hardhat: { chainId: 31337 },
